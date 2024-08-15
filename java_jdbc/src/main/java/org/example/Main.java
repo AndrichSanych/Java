4209f9ab41060554d9f1fafe3199d3ffd170b15b
AndrichSanych <Prima_rq56@itstep.student.org>
2024-08-15 22:32:32 +0300

    Homework 2

--- java_jdbc/src/main/java/org/example/Main.java
+++ java_jdbc/src/main/java/org/example/Main.java
@@ -12,34 +12,21 @@ public class Main {
 
     public static void main(String[] args)  {
         try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
             //createTable();
-
-
             Abonent ivan = new Abonent();
             ivan.setName("Підкаблучник Іван Васильович");
             ivan.setPhoneNumber("098 78 67 567");
             ivan.setAddress("м. Рівне вул. Соборна 24, кв.45");
             ivan.setEmail("ivan@gmail.com");
+//
+//          addContact(ivan);
 
-            String insertSQL = "INSERT INTO Phonebook (Name, PhoneNumber, Email, Address) VALUES (?, ?, ?, ?);";
-            PreparedStatement preparedStatement = null;
+            ivan.setPhoneNumber("0000");
+            updateContact(1, ivan);
 
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            // Створення PreparedStatement для виконання SQL-запиту
-            preparedStatement = connection.prepareStatement(insertSQL);
+            //deleteContact(4);
 
-            // Встановлення значень для параметрів запиту
-            preparedStatement.setString(1, ivan.getName());
-            preparedStatement.setString(2, ivan.getPhoneNumber());
-            preparedStatement.setString(3, ivan.getEmail());
-            preparedStatement.setString(4, ivan.getAddress());
-
-            // Виконання SQL-запиту
-            int rowsInserted = preparedStatement.executeUpdate();
-
-            if (rowsInserted > 0) {
-                System.out.println("A new contact was inserted successfully.");
-            }
         } catch (SQLException e) {
             System.out.println("Щось пішло не так! " + e.getMessage());
         }
@@ -73,4 +60,58 @@ public class Main {
                 connection.close();
         }
     }
+
+    public static void addContact(Abonent abonent) throws SQLException {
+        String insertSQL = "INSERT INTO Phonebook (Name, PhoneNumber, Email, Address) VALUES (?, ?, ?, ?);";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setString(1, abonent.getName());
+            preparedStatement.setString(2, abonent.getPhoneNumber());
+            preparedStatement.setString(3, abonent.getEmail());
+            preparedStatement.setString(4, abonent.getAddress());
+
+            int rowsInserted = preparedStatement.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("Новий контакт доданий успішно.");
+            }
+        }
+    }
+
+    public static void updateContact(int id, Abonent abonent) throws SQLException {
+        String updateSQL = "UPDATE Phonebook SET Name = ?, PhoneNumber = ?, Email = ?, Address = ? WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+            preparedStatement.setString(1, abonent.getName());
+            preparedStatement.setString(2, abonent.getPhoneNumber());
+            preparedStatement.setString(3, abonent.getEmail());
+            preparedStatement.setString(4, abonent.getAddress());
+            preparedStatement.setInt(5, id);
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.println("Контакт оновлено успішно.");
+            } else {
+                System.out.println("Контакт з таким ID не знайдений.");
+            }
+        }
+    }
+
+    public static void deleteContact(int id) throws SQLException {
+        String deleteSQL = "DELETE FROM Phonebook WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
+            preparedStatement.setInt(1, id);
+
+            int rowsDeleted = preparedStatement.executeUpdate();
+
+            if (rowsDeleted > 0) {
+                System.out.println("Контакт видалений успішно.");
+            } else {
+                System.out.println("Контакт з таким ID не знайдений.");
+            }
+        }
+    }
+
+
+
+
 }
