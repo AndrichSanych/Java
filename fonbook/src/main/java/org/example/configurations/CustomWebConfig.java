766c99225a2b22dc6c089a2c4fbc3bc65210814b
AndrichSanych <Prima_rq56@itstep.student.org>
2024-09-16 20:56:36 +0300

    Working app

--- fonbook/src/main/java/org/example/configurations/CustomWebConfig.java
+++ fonbook/src/main/java/org/example/configurations/CustomWebConfig.java
@@ -1,4 +1,20 @@
 package org.example.configurations;
 
-public class CustomWebConfig {
+import lombok.AllArgsConstructor;
+import org.example.services.StorageProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@AllArgsConstructor
+public class CustomWebConfig implements WebMvcConfigurer {
+    private final StorageProperties storageProperties;
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        String location = storageProperties.getLocation();
+        registry.addResourceHandler("/"+location+"/**")
+                .addResourceLocations("file:"+location+"/");
+    }
 }
